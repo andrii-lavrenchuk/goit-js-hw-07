b7894f9ada40824c38831008d29eba8347ffd424
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-09-16 19:02:42 +0300

    Adds task-2.js

--- scripts/task-1.js
+++ scripts/task-1.js
@@ -2,14 +2,14 @@ const allCategoriesEl = document.querySelector('#categories');
 
 console.log(`В списке ${allCategoriesEl.children.length} категории`);
 
-const allItemEl = document.querySelectorAll('.item');
+const allItemEl = allCategoriesEl.querySelectorAll('.item');
 
 allItemEl.forEach(element => {
   const titleEl = element.firstElementChild.textContent;
   const ulEl = element.lastElementChild;
-  const amounOfElt = ulEl.children.length;
+  const amounOfEl = ulEl.children.length;
 
   console.log(`
-Категория: ${titleEl}
-Kоличество элементов: ${amounOfElt}`);
+  Категория: ${titleEl}
+  Kоличество элементов: ${amounOfEl}`);
 });
---
+++ scripts/task-2.js
@@ -0,0 +1,27 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingredients[i];
+//   elements.push(liEl);
+// }
+// ingredientsEl.append(...elements);
+
+const elements = ingredients.map(name => {
+  const liEl = document.createElement('li');
+  liEl.textContent = name;
+
+  return liEl;
+});
+
+ingredientsEl.append(...elements);
