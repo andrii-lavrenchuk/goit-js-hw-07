d2eeb924987357d17fc13a91ff460e81b74d06bf
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-09-17 12:27:40 +0300

    Adds css styles and task-2.js

---
+++ scripts/task-3.js
@@ -0,0 +1,34 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryListEl = document.querySelector('#gallery');
+
+const galeryOfImages = images.map(({ url, alt }) => {
+  const imageList = document.createElement('li');
+
+  const imageEl = document.createElement('img');
+
+  imageEl.src = url;
+  imageEl.alt = alt;
+
+  imageList.classList.add('flex-box');
+  imageList.appendChild(imageEl);
+
+  return imageList;
+});
+galleryListEl.append(...galeryOfImages);
