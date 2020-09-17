6382373ad65a86d2eb713ff5b3fb7da71996426b
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-09-17 23:25:11 +0300

    Update task-6

--- scripts/task-6.js
+++ scripts/task-6.js
@@ -1,20 +1,5 @@
 const inputEl = document.querySelector('#validation-input');
-// console.log(inputEl.value === inputEl.dataset.length);
 
-// const getOutOfFocus = event => {
-//   if (event.target.value.length === Number(inputEl.dataset.length)) {
-//     inputEl.classList.add('valid');
-//     inputEl.classList.remove('invalid');
-//   } else if (event.target.value.length !== Number(inputEl.dataset.length)) {
-//     inputEl.classList.add('invalid');
-//     inputEl.classList.remove('valid');
-//   } else {
-//     inputEl.classList.remove('valid');
-//     inputEl.classList.remove('invalid');
-//   }
-// };
-
-// inputEl.addEventListener('blur', getOutOfFocus);
 const getOutOfFocus = event => {
   if (event.target.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add('valid');
@@ -29,18 +14,3 @@ const getOutOfFocus = event => {
 };
 
 inputEl.addEventListener('blur', getOutOfFocus);
-
-// e
-// if (event.target.value.length === Number(inputEl.dataset.length)) {
-//   inputEl.classList.add('valid');
-// } else if (event.target.value.length !== Number(inputEl.dataset.length)) {
-//   inputEl.classList.remove('valid');
-//   inputEl.classList.add('invalid');
-// }
-//
-//   console.log('Скільки юзер ввів', event.target.value.length);
-//   console.log('Скільки треба ввести символів ', inputEl.dataset.length);
-//   console.log(
-//     'Чи рівні значення ',
-//     Number(inputEl.dataset.length) === event.target.value.length,
-//   );
